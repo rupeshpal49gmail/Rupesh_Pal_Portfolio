e8ba57b71585702d9c8a022f9730836750e4ad70
Rupesh Pal <rupeshpal49@gmail.com>
2024-11-23 23:55:19 +0530

    Add navbar page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 // import "./App.css";
 import Homepage from "./Components/Homepage";
+import Navbar from "./Components/Navbar";
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Homepage />
     </>
   );
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -3,7 +3,21 @@ import Navbar from "./Navbar";
 function Homepage() {
   return (
     <div>
-      <Navbar />
+      <div className="container home">
+        <div className="left">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+            natus!
+          </h1>
+          <a href="" className="btn btn-outline-warning">
+            Download
+          </a>
+        </div>
+
+        <div className="right">
+          <h3>Lorem ipsum dolor sit amet.</h3>
+        </div>
+      </div>
     </div>
   );
 }
