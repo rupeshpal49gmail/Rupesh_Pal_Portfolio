62fb65548ab3d89e29b1e27e0e9226cddf9f72fd
Rupesh Pal <rupeshpal49@gmail.com>
2025-01-01 23:51:43 +0530

    Add style to project section

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,28 +4,43 @@ import project from "./data/projects.json";
 const Projects = () => {
   return (
     <>
-      <div className="container projects my-3">
+      <div className="container projects my-3" id="projects">
         <h1>PROJECTS</h1>
-        <div className="row display-flex justify-center align-content-center">
+        <div className="row display-flex justify-content-center align-content-center">
           {project.map((data) => (
             <>
               <div
                 key={data.id}
-                className="my-3 col-sm-3 col-md-4 col-lg-4 col-xl-3"
+                className="my-4 col-sm-6 col-md-4 col-lg-3 cmx-4"
               >
                 <div
                   className="card bg-dark text-light"
-                  style={{ width: "18rem" }}
+                  style={{
+                    width: "18rem",
+                    border: "1px solid grey",
+                    boxShadow: "5px 5px 10px rgba(101, 175, 10, 0.5)",
+                  }}
+                  data-aos="flip-right"
+                  data-aos-duration="1000"
                 >
-                  <img
-                    src={data.imageSrc}
-                    className="card-img-top"
-                    alt="img-break"
-                  />
+                  <div className="img d-flex justify-content-center align-content-center p-3">
+                    <img
+                      src={data.imageSrc}
+                      className="card-img-top"
+                      alt="img-break"
+                      style={{
+                        width: "250px",
+                        height: "200px",
+                        border: "2px solid yellow",
+                        borderRadius: "10px",
+                        background: "#f1f1f1",
+                      }}
+                    />
+                  </div>
                   <div className="card-body text-center">
                     <h5 className="card-title">{data.title}</h5>
                     <p className="card-text">{data.description}</p>
-                    <a href="#" className="btn btn-primary">
+                    <a href="{data.demo}" className="btn btn-primary">
                       Go Somewhere
                     </a>
                   </div>
